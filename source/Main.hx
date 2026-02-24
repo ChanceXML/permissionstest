f86b7312d24f9dd6a99f8e8b9a9082c67a6c4f91
ChanceXML <joseguest1337@gmail.com>
2026-02-24 11:59:51 -0700

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,47 +1,40 @@
-package;
-
-import openfl.display.Sprite;
-import openfl.events.Event;
-import openfl.text.TextField;
-import openfl.text.TextFormat;
-import sys.FileSystem;
+#if !macro
+#if TOUCH_CONTROLS
+import flixel.FlxCamera;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.math.FlxPoint;
+import flixel.util.FlxColor;
+import flixel.util.FlxDestroyUtil;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.sound.FlxSound;
+import funkin.backend.assets.ModsFolder;
+import funkin.backend.assets.Paths;
+import funkin.backend.utils.CoolUtil;
+import funkin.backend.utils.NativeAPI;
+import funkin.options.Options;
+import mobile.objects.TouchButton;
+import mobile.objects.TouchPad;
+import mobile.funkin.backend.utils.MobileData;
+import mobile.funkin.backend.system.input.MobileInputID;
+import mobile.funkin.backend.system.input.MobileInputManager;
+#end
+import mobile.funkin.backend.utils.StorageUtil;
 
 #if android
-import openfl.permissions.Permissions;
+import android.content.Context as AndroidContext;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
 #end
 
-class Main extends Sprite {
+#if sys
+import sys.FileSystem;
+import sys.io.File;
+#end
 
-    public function new() {
-        super();
-        addEventListener(Event.ADDED_TO_STAGE, init);
-    }
-
-    function init(e:Event):Void {
-        removeEventListener(Event.ADDED_TO_STAGE, init);
-
-        #if android
-        Permissions.requestPermission("android.permission.WRITE_EXTERNAL_STORAGE");
-        #end
-
-        var path = "/storage/emulated/0/TestStorageFolder";
-
-        try {
-            if (!FileSystem.exists(path)) {
-                FileSystem.createDirectory(path);
-            }
-            showText("SUCCESS\nFolder created.");
-        } catch (e:Dynamic) {
-            showText("FAILED\n" + e);
-        }
-    }
-
-    function showText(msg:String):Void {
-        var tf = new TextField();
-        tf.defaultTextFormat = new TextFormat("_sans", 28, 0xFFFFFF);
-        tf.width = stage.stageWidth;
-        tf.height = stage.stageHeight;
-        tf.text = msg;
-        addChild(tf);
-    }
-}
+using StringTools;
+#end
